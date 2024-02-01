ccf361a9ccac683b21e6b953fa411f630746bdf5
James Coxon <175479+jamescoxon@users.noreply.github.com>
2024-02-01 19:48:35 +0000

    Create settings.py
    example settings.py

---
+++ settings.py
@@ -0,0 +1,15 @@
+cost_raw = 200000000000000000000000000000
+
+host_number = '123456789'
+host_nano_address = 'nano_'
+
+rep_number = '123456789'
+rep_nano_address = 'nano_'
+
+debug_number = '123456789'
+debug_nano_address = 'nano_'
+
+wallet_seed =''
+nano_address = 'nano_'
+index = 0
+api_key = '0'
